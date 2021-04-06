c42d2808b01bae7ac11f7cf446c510fdc7d6f327
Banjamolly <humphries0221@gmail.com>
2021-04-06 16:43:27 +0100

    Initial author routes

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+// What files are needed
 const express = require('express')
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 
-//Routers
+
+//Routers call up
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
+//Tell them what files to use
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 // You never want to hardcode your connection because it will change
@@ -25,6 +31,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//When to use the routers
 app.use('/', indexRouter) //for url '/' use indexRouter
+app.use('/authors', authorRouter) 
 
 app.listen(process.env.PORT || 3000)

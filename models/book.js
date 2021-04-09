f1cbbcdc624a2c63bb3d95d5a3bc48f7d8337e5a
Banjamolly <humphries0221@gmail.com>
2021-04-09 10:29:03 +0100

    changed file upload format

--- models/book.js
+++ models/book.js
@@ -1,59 +1,4 @@
-// const mongoose = require('mongoose');
-// const path = require('path');
-
-// const coverImageBasePath = 'uploads/bookCovers'
-
-// const bookSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String
-//     },
-//     publishDate: {
-//         type: Date,
-//         required: true
-//     },
-//     pageCount: {
-//         type: Number,
-//         required: true
-//     },
-//     createdAt: {
-//         type: Date,
-//         required: true,
-//         default: Date.now
-//     },
-//     coverImageName: {
-//         type: String,
-//         required: true
-//     },
-//     author: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         // The above simply tells Mongoose to reference another collection.
-//         required: true,
-//         ref: 'Author'
-//         // The above must match the module.exports name on the referenced model
-//     }
-
-// })
-
-// bookSchema.virtual('coverImagePath').get(function(){
-//     // a function() is used instead of () => {} because we need to use this.object
-//     if(this.coverImageName != null){
-//         return path.join('/', coverImageBasePath, this.coverImageName)
-//     }
-// })
-// // Virtual is like the properties above except that it derives its value from the paths above
-
-// // 'Book' is essentialy the name of the table
-// module.exports = mongoose.model('Book', bookSchema)
-// module.exports.coverImageBasePath = coverImageBasePath
-
 const mongoose = require('mongoose')
-const path = require('path')
-
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -76,7 +21,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true
+  },
+  coverImageType: {
     type: String,
     required: true
   },
@@ -88,10 +37,11 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-  if (this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
   }
 })
 
+
+
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
--- routes/books.js
+++ routes/books.js
@@ -1,122 +1,12 @@
-// const express = require('express')
-// const router = express.Router()
-// const multer = require('multer')
-// const Book = require('../models/book')
-// const fs = require('fs')
-// // Above is file system and used in our case to remove saved cover photos when there was an error on the form
-// const path = require('path')
-// // The above is required so we can access the path to the files that are saved for the cover upload. See below line
-// const uploadPath = path.join('public', Book.coverImageBasePath) 
-// // const upldPath = require('../public/uploads/bookCovers') //remove
-// const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-// // The above is an array of all the image types that we accept
-// const Author = require('../models/author')
-// const { title } = require('process')
-// const upload = multer({
-//     dest: uploadPath, 
-//     fileFilter: (req, file, callback) => {
-//         callback(null, imageMimeTypes.includes(file.mimetype) )
-//     }
-// })
 
-// //All books route
-// // the '/' gets appended onto the route declared in server.js (/books)
-// router.get('/', async (req, res) => {
-//     let query = Book.find()
-//     if(req.query.title != null && req.query.title != ''){
-//         query = query.regex('title', new RegExp(req.query.title, 'i'))
-//     }
-//     if(req.query.publishedBefore != null && req.query.publishedBefore != ''){
-//         query = query.lte('publishDate', req.query.publishedBefore)
-//         // lte = less than or equal to
-//     }
-//     if(req.query.publishedAfter != null && req.query.publishedAfter != ''){
-//         query = query.gte('publishDate', req.query.publishedAfter)
-//         // gte >=
-//     }
-//     try{
-//         const books = await query.exec()
-//         res.render('books/index', {
-//             books: books,
-//             searchOptions: req.query
-    
-//         })
-//     } catch {
-//         res.redirect('/')
-//     }
-// })
-
-// //New book route
-// router.get('/new', async (req, res) => {
-//     renderNewPage(res, new Book())
-// })
-
-// //Create new book route
-// router.post('/', upload.single('cover'), async (req, res) => {
-//     // 'cover' is the name of the input variable on the form
-//     const fileName = req.file != null ? req.file.filename : null
-//     const book = new Book({
-//         title: req.body.title,
-//         author: req.body.author,
-//         publishDate: new Date(req.body.publishDate),
-//         // Above is a new Date() because we are getting a string because we had to to remove the time.
-//         pageCount: req.body.pageCount,
-//         // cover: req.body.cover, we first need to create the file image on our file, get the name from that, and then save that into our Book object. Easiest way to do that is to use a library called multer. see below
-//         coverImageName: fileName,
-//         description: req.body.description
-//     })
-//     try{
-//         const newBook = await book.save()
-//         res.redirect(`books`)
-        
-//     } catch {
-
-//         book.coverImageName && removeBookCover(book.coverImageName) 
-//         renderNewPage(res, book, hasError = true)
-//     }
-// })
-
-// function removeBookCover(fileName){
-//     fs.unlink(path.join(uploadPath, fileName), err => {
-//         if(err) console.error(err)
-//     })
-// }
-
-// async function renderNewPage(res, book, hasError = false){
-//     try{
-//         const authors = await Author.find({})
-//         // The above is so that if they imput data and we reject it then it autopopulates the fields with the data they already gave.
-//         const params = {
-//             authors: authors,
-//             book: book
-//         }
-//         if(hasError) params.errorMessage = 'Error creating book'
-//         res.render('books/new', params)
-//     } catch {
-//         res.redirect('/books')
-//     }
-// }
-
-
-// module.exports = router
 
 
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
-const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-      callback(null, imageMimeTypes.includes(file.mimetype))
-    }
-  })
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -147,47 +37,51 @@ router.get('/new', async (req, res) => {
 })
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null
+router.post('/',  async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
     description: req.body.description
   })
+  saveCover(book, req.body.cover)
   
   try {
     const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
     res.redirect('books')
   } catch {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName)
-    }
+    // if (book.coverImageName != null) {
+    //   removeBookCover(book.coverImageName)
+    // }
     renderNewPage(res, book, true)
   }
 })
 
-function removeBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), err => {
-    if (err) console.error(err)
-  })
-}
+
 
 async function renderNewPage(res, book, hasError = false) {
-  try {
-    const authors = await Author.find({})
-    const params = {
-      authors: authors,
-      book: book
+    try {
+        const authors = await Author.find({})
+        const params = {
+            authors: authors,
+            book: book
+        }
+        if (hasError) params.errorMessage = 'Error Creating Book'
+        res.render('books/new', params)
+    } catch {
+        res.redirect('/books')
     }
-    if (hasError) params.errorMessage = 'Error Creating Book'
-    res.render('books/new', params)
-  } catch {
-    res.redirect('/books')
-  }
+}
+
+function saveCover(book, coverEncoded){
+    if(coverEncoded == null)return
+    const cover = JSON.parse(coverEncoded)
+    if(coverEncoded != null && imageMimeTypes.includes(cover.type)){
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
 }
 
 module.exports = router

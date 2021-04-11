2fce62efd5f197293daaba177e359336077afad8
Banjamolly <humphries0221@gmail.com>
2021-04-11 12:42:19 +0100

    Finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginFileEncode,
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-)
+const rootStyles = window.getComputedStyle(document.documentElement);
+// This will get all the styles from the root element of our document (because css loads las this has to be done now). The root styles are all the styles inside the :root tag in the css files. 
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else{
+    document.getElementById('main-css')
+    .addEventListener('load',ready)
+}
+// Above checks if the document is loaded and if not it waits for it to 'load' and then calls the ready function
 
-FilePond.parse(document.body);
+function ready(){
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth/coverAspectRatio
+
+    FilePond.registerPlugin(
+        FilePondPluginFileEncode,
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        )
+        
+        FilePond.setOptions({
+            stylePanelAspectRatio: 1/coverAspectRatio,
+            imageResizeTargetWidth: coverWidth,
+            imageResizeTargetHeight: coverHeight
+        })
+        
+        FilePond.parse(document.body);
+    }
